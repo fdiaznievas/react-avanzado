2e5228d2890c881a1b006e2e77fe94cec16e442c
fdiaznievas <diaznievasfabricio@gmail.com>
2025-06-15 00:13:07 -0300

    Agrego conexión a base de datos. Agrego CORS y middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import sequelize from './db.js';
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('../frontend'));
+
+// Iniciar la base de datos
+// sequelize.sync({force: true});
+sequelize.sync();
 
 app.listen(PORT, ()=>{
     console.log(`El servidor está escuchando en el puerto ${PORT}`)
